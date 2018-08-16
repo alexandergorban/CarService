87bd5a9b845bfc8c8cc909f1f423eae7df3a2c56
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-08-16 05:26:41 +0300

    Added gulp and gulp-less

---
+++ CarService/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function () {
+    return gulp.src('Styles/site.less') // only compile the entry file
+        .pipe(less())
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('Styles/*.less', ['less']);  // Watch all the .less files, then run the less task
+});
+
+gulp.task('default', ['watch']); // Default will run the 'entry' watch task
