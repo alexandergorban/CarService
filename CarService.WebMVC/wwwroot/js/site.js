6e967f2adc6df95bbbc134d416d9ef91ab68b9c2
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-08-19 06:40:13 +0300

    Cleanup site.js

--- CarService.WebMVC/wwwroot/js/site.js
+++ CarService.WebMVC/wwwroot/js/site.js
@@ -8,12 +8,10 @@ var navbar = document.getElementById("navbar");
 var navbarCollapse = document.getElementById("navbar_collapse");
 
 // Get the offset position of the navbar
-//var sticky = navbar.offsetTop;
 var sticky = 85;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function fixedNavBar() {
-    //debugger;
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
         navbarCollapse.classList.add("sticky");
